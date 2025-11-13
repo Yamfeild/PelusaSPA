f2340e854dbe366b2f8251e95b6676c27e4fe78f
stiven <stiven.jimenez@unl.edu.ec>
2025-11-13 14:40:00 -0500

    Documentacion

--- PelusaSPA/usuario_service/usuario_service/urls.py
+++ PelusaSPA/usuario_service/usuario_service/urls.py
@@ -20,12 +20,21 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # OpenAPI schema y documentación
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
 ]
 
