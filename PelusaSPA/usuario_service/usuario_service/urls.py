70dcb779c3ea71abf4f93675fbd473320ccbdbdb
stiven <stiven.jimenez@unl.edu.ec>
2025-11-13 19:49:16 -0500

    Doc

--- PelusaSPA/usuario_service/usuario_service/urls.py
+++ PelusaSPA/usuario_service/usuario_service/urls.py
@@ -25,12 +25,24 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
+from drf_spectacular.utils import extend_schema
+
+@extend_schema(exclude=True)
+class HiddenTokenObtainPairView(TokenObtainPairView):
+    pass
+
+
+@extend_schema(exclude=True)
+class HiddenTokenRefreshView(TokenRefreshView):
+    pass
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Endpoints JWT "puros" ocultos del esquema para evitar confusión
+    path('api/token/', HiddenTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', HiddenTokenRefreshView.as_view(), name='token_refresh'),
     # OpenAPI schema y documentación
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
